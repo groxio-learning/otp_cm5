2140a8b57cee3cf92b4ea15a5604b970ed7a06aa
Mohan Sai Koneru <mohan.koneru@caremore.com>
2022-09-29 10:29:30 -0400

    initial mindblower application

---
+++ mindblower/lib/mindblower.ex
@@ -0,0 +1,18 @@
+defmodule Mindblower do
+  @moduledoc """
+  Documentation for `Mindblower`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Mindblower.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
